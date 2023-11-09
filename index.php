d39d122b0fb13e5c7311cf1ec7814a887f64fca3
proyectoII <rag_191@hotmail.com>
2023-11-09 15:43:57 -0600

    segunda carga

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Página sin título</title>
+
+<link href="Aplicación_web_con_base_de_datos.css" rel="stylesheet">
+<link href="index.css" rel="stylesheet">
+<script src="jquery-3.6.0.min.js"></script>
+</head>
+<body>
+<div id="container">
+
+<iframe id="Admin1" name="Contraseña del Administrador" style="position:absolute;left:17px;top:383px;width:937px;height:369px;z-index:1;" src="Contraseña del Administrador.php"></iframe>
+<div id="wb_Text1" style="position:absolute;left:190px;top:85px;width:590px;height:30px;text-align:center;z-index:2;">
+<span style="color:#FFFFFF;font-family:Arial;font-size:27px;"><strong>ZONA DEL ADMINISTRADOR</strong></span></div>
+</div>
+</body>
+</html>
